2548687cb45100f87b41df1aafe233af23a51571
bmoreira <bmoreira@student.42sp.org.br>
2025-12-04 03:37:17 -0300

    feat: validate if arguments are greater than 0

--- src/validate_input.c
+++ src/validate_input.c
@@ -6,7 +6,7 @@
 /*   By: bmoreira <bmoreira@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/04 01:50:24 by bmoreira          #+#    #+#             */
-/*   Updated: 2025/12/04 03:16:04 by bmoreira         ###   ########.fr       */
+/*   Updated: 2025/12/04 03:35:28 by bmoreira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,22 +24,14 @@ void	validate_arguments(char **argv)
 {
 	int	i;
 
-	i = 1;
-	while (argv[i])
-		if (ft_atol(argv[i++]) > INT_MAX)
+	i = 0;
+	while (argv[++i])
+		if (ft_atol(argv[i]) <= 0 || ft_atol(argv[i]) > INT_MAX)
 			error_handler("Invalid argument provided.", EXIT_FAILURE);	
-}
-
-void	validate_philos(char **argv)
-{
-	if (ft_atol(argv[1]) <= 0 || ft_atol(argv[1]) > 200)
-		error_handler("Invalid number of philosophers. "
-			"Provide a number between 1 and 200.", EXIT_FAILURE);
 }
 
 void	validate_input(int argc, char **argv)
 {
 	validate_quantity(argc);
 	validate_arguments(argv);
-	validate_philos(argv);
 }
