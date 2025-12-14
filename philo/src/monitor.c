1b56cc0c790cad11574f4de582ad06fd3a2f57f6
babidrummond <babidrummond@hotmail.com>
2025-12-13 23:17:49 -0300

    feat: refactor monitor to make is_someone_dead and is_all_eaten more independent and modular

--- philo/src/monitor.c
+++ philo/src/monitor.c
@@ -6,39 +6,53 @@
 /*   By: bmoreira <bmoreira@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/09 19:47:37 by bmoreira          #+#    #+#             */
-/*   Updated: 2025/12/11 21:55:24 by bmoreira         ###   ########.fr       */
+/*   Updated: 2025/12/13 23:12:25 by bmoreira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
-static int	is_someone_dead(t_philo *philo)
+static int	is_someone_dead(t_table *table)
 {
+	int		i;
 	long	now;
 	long	next_meal;
 
-	pthread_mutex_lock(&philo->table->meal_lock);
-	now = get_time_now();
-	next_meal = now - philo->last_meal;
-	pthread_mutex_unlock(&philo->table->meal_lock);
-	if (next_meal >= philo->table->time_to_die)
+	i = -1;
+	while (++i < table->num_philos)
 	{
-		pthread_mutex_lock(&philo->table->death_lock);
-		philo->table->is_running = FALSE;
-		pthread_mutex_unlock(&philo->table->death_lock);
-		pthread_mutex_lock(&philo->table->print_lock);
-		printf("\033[90m%ld %d has died 💀\033[0m\n",
-			now - philo->table->start_time, philo->philo_id);
-		pthread_mutex_unlock(&philo->table->print_lock);
-		return (TRUE);
+		pthread_mutex_lock(&table->meal_lock);
+		now = get_time_now();
+		next_meal = now - table->philos[i].last_meal;
+		pthread_mutex_unlock(&table->meal_lock);
+		if (next_meal >= table->time_to_die)
+		{
+			pthread_mutex_lock(&table->death_lock);
+			table->is_running = FALSE;
+			pthread_mutex_unlock(&table->death_lock);
+			pthread_mutex_lock(&table->print_lock);
+			printf("\033[90m%ld %d has died 💀\033[0m\n",
+				now - table->start_time, table->philos[i].philo_id);
+			pthread_mutex_unlock(&table->print_lock);
+			return (TRUE);
+		}
 	}
 	return (FALSE);
 }
 
-static int	is_all_eaten(t_table *table, int all_eaten)
+static int	is_all_eaten(t_table *table)
 {
-	if (table->max_meals > 0)
+	int	i;
+	int	all_eaten;
+
+	i = -1;
+	all_eaten = 0;
+	while (++i < table->num_philos)
 	{
+		pthread_mutex_lock(&table->meal_lock);
+		if (table->philos[i].meals_eaten == table->max_meals)
+			all_eaten++;
+		pthread_mutex_unlock(&table->meal_lock);
 		if (all_eaten == table->num_philos)
 		{
 			pthread_mutex_lock(&table->death_lock);
@@ -56,23 +70,12 @@ static int	is_all_eaten(t_table *table, int all_eaten)
 
 void	start_monitor(t_table *table)
 {
-	int		i;
-	int		all_eaten;
-
 	while (is_simulation_running(table))
 	{
-		i = -1;
-		all_eaten = 0;
-		while (++i < table->num_philos)
-		{
-			pthread_mutex_lock(&table->meal_lock);
-			if (table->philos[i].meals_eaten == table->max_meals)
-				all_eaten++;
-			pthread_mutex_unlock(&table->meal_lock);
-			if (is_someone_dead(&table->philos[i])
-				|| is_all_eaten(table, all_eaten))
-				break ;
-		}
+		if (is_someone_dead(table))
+			break ;
+		if (table->max_meals > 0 && is_all_eaten(table))
+			break ;
 		usleep(50);
 	}
 }
